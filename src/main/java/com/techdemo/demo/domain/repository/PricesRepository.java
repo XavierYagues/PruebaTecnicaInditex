7cebb0c9dc3205031f3cfa59f70723293ebb932d
Xavi Yagües <xavi.yagues@gmail.com>
2024-10-08 13:37:57 +0200

    final commit

--- src/main/java/com/techdemo/demo/domain/repository/PricesRepository.java
+++ src/main/java/com/techdemo/demo/domain/repository/PricesRepository.java
@@ -1,8 +1,10 @@
 package com.techdemo.demo.domain.repository;
 
 import com.techdemo.demo.domain.model.Prices;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface PricesRepository {
+public interface PricesRepository extends JpaRepository <Prices, Long>{
     List<Prices> findByProductIdAndBrandId(Long productId, Integer brandId);
 }
